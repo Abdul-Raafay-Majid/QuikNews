a4fd4a8d3d5b7a3d8fe7556ca9773e2f8a8dfb27
Abdul-Raafay_Majid <majidraafay9@gmail.com>
2022-10-17 19:18:31 +0500

    All Important News Sections Added

---
+++ app/src/main/java/com/example/quiknews/presentation/welcome_screen/WelcomeScreen.kt
@@ -0,0 +1,27 @@
+package com.example.quiknews.presentation.welcome_screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun WelcomeScreen(
+    modifier: Modifier=Modifier
+){
+
+
+}
+
+@Composable
+fun ChooseInterestItem(sections:List<>){
+
+}
+
+@Preview
+@Composable
+fun PreviewHomeScreen(){
+    WelcomeScreen()
+}
+
+
+
