3add0deb706c646417a9f5c4ae3c68c731d89ee3
Abdul-Raafay_Majid <majidraafay9@gmail.com>
2022-10-22 14:28:01 +0500

    Splash Screen Added

--- app/src/main/java/com/example/quiknews/MainActivity.kt
+++ app/src/main/java/com/example/quiknews/MainActivity.kt
@@ -5,42 +5,45 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.quiknews.domain.model.ArticleDto
-import com.example.quiknews.presentation.NewsWire.ArticleItem
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.quiknews.presentation.NewsWire.NewsWireScreen
 import com.example.quiknews.presentation.NewsWireEvent
 import com.example.quiknews.presentation.NewsWireViewModel
 import com.example.quiknews.ui.theme.QuikNewsTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val viewModel:NewsWireViewModel by viewModels()
+    private val viewModel: NewsWireViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            this.setKeepOnScreenCondition{
+                viewModel.isClearing.value
+            }
+        }
         setContent {
-            Log.d("Clean Up","onDestroy")
-            viewModel.getNewsWireUseCases(NewsWireEvent.ClearAllArticles)
+            Log.d("Clean Up", "onDestroy")
             QuikNewsTheme {
                 // A surface container using the 'background' color from the theme
+                Box(
+                    modifier= Modifier.fillMaxSize()
+                ) {
                     NewsWireScreen()
                 }
             }
         }
+    }
 
 
 }
-
-
 
 
 @Composable
--- app/src/main/java/com/example/quiknews/presentation/NewsWireViewModel.kt
+++ app/src/main/java/com/example/quiknews/presentation/NewsWireViewModel.kt
@@ -10,11 +10,8 @@ import com.example.quiknews.R
 import com.example.quiknews.core.utils.Resource
 import com.example.quiknews.data.NewsRepoImpl
 import com.example.quiknews.domain.NewsWireUseCase
-import com.example.quiknews.presentation.utils.Section
 import com.example.quiknews.presentation.utils.Sections
-import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Delay
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -24,101 +21,108 @@ import javax.inject.Inject
 @HiltViewModel
 class NewsWireViewModel @Inject constructor(
     private val newsWireUseCase: NewsWireUseCase,
-    private val newsRepo:NewsRepoImpl
+    private val newsRepo: NewsRepoImpl
 ) :
     ViewModel() {
 
     private val _newsWireState = mutableStateOf(NewsWireState())
-    val newsWireState= this._newsWireState
+    val newsWireState = this._newsWireState
 
-
-
-
-
-
-
+    private val _isClearing = mutableStateOf(true)
+    val isClearing = _isClearing
 
     init {
-        getNewsWireUseCases(NewsWireEvent.GetArticles(1))
+        getNewsWireUseCases(NewsWireEvent.ClearAllArticles)
+        getNewsWireUseCases(NewsWireEvent.GetArticles(0))
     }
 
-     fun getNewsWireUseCases(newsWireEvent: NewsWireEvent) {
+    fun getNewsWireUseCases(newsWireEvent: NewsWireEvent) {
         when (newsWireEvent) {
             is NewsWireEvent.GetArticles -> {
                 viewModelScope.launch {
-                    if(_newsWireState.value.newsWireArticles!=null)
-                        _newsWireState.value=newsWireState.value.copy(
+                    if (_newsWireState.value.newsWireArticles != null)
+                        _newsWireState.value = newsWireState.value.copy(
                             newsWireArticles = null,
                             isLoading = true,
-                            section ="",
+                            section = "",
                             error = null
                         )
                     delay(500L)
-                    newsWireUseCase.getNewsWireData("all", Sections.sections[newsWireEvent.page].pathParam)
+                    newsWireUseCase.getNewsWireData(
+                        "all",
+                        Sections.sections[newsWireEvent.page].pathParam
+                    )
                         .onEach { result ->
                             when (result) {
                                 is Resource.Success -> {
-                                    this@NewsWireViewModel.newsWireState.value = this@NewsWireViewModel.newsWireState.value.copy(
-                                        newsWireArticles =result.data?.articles,
-                                        isLoading = false,
-                                        section = result.data?.section
-                                    )
+                                    this@NewsWireViewModel.newsWireState.value =
+                                        this@NewsWireViewModel.newsWireState.value.copy(
+                                            newsWireArticles = result.data?.articles,
+                                            isLoading = false,
+                                            section = result.data?.section
+                                        )
                                 }
 
                                 is Resource.Loading -> {
-                                    this@NewsWireViewModel.newsWireState.value = this@NewsWireViewModel.newsWireState.value.copy(
-                                        newsWireArticles = result.data?.articles,
-                                        isLoading = true,
-                                        section = result.data?.section
-                                    )
+                                    this@NewsWireViewModel.newsWireState.value =
+                                        this@NewsWireViewModel.newsWireState.value.copy(
+                                            newsWireArticles = result.data?.articles,
+                                            isLoading = true,
+                                            section = result.data?.section
+                                        )
                                 }
 
                                 is Resource.Error -> {
-                                    this@NewsWireViewModel.newsWireState.value = this@NewsWireViewModel.newsWireState.value.copy(
-                                        error = result.message
-                                    )
+                                    this@NewsWireViewModel.newsWireState.value =
+                                        this@NewsWireViewModel.newsWireState.value.copy(
+                                            error = result.message
+                                        )
                                 }
                             }
                         }.launchIn(this)
                 }
             }
 
-            is NewsWireEvent.ClearAllArticles->{
+            is NewsWireEvent.ClearAllArticles -> {
                 viewModelScope.launch {
                     newsWireUseCase.deleteAllArticles()
+                    _isClearing.value = false
                 }
             }
 
-            is NewsWireEvent.RefreshArticles->{
+            is NewsWireEvent.RefreshArticles -> {
                 viewModelScope.launch {
-                    if(_newsWireState.value.newsWireArticles!=null)
-                        _newsWireState.value=newsWireState.value.copy(
+                    if (_newsWireState.value.newsWireArticles != null)
+                        _newsWireState.value = newsWireState.value.copy(
                             newsWireArticles = null,
                             isLoading = true,
-                            section ="",
+                            section = "",
                             error = null
                         )
-                    newsWireUseCase.refreshArticle("all",newsWireEvent.section).onEach { result->
-                        when(result){
-                            is Resource.Loading->{
-                                this@NewsWireViewModel.newsWireState.value = this@NewsWireViewModel.newsWireState.value.copy(
-                                    isLoading = true,
-                                    section = result.data?.section
-                                )
+                    newsWireUseCase.refreshArticle("all", newsWireEvent.section).onEach { result ->
+                        when (result) {
+                            is Resource.Loading -> {
+                                this@NewsWireViewModel.newsWireState.value =
+                                    this@NewsWireViewModel.newsWireState.value.copy(
+                                        isLoading = true,
+                                        section = result.data?.section
+                                    )
                             }
 
-                            is Resource.Error->{
-                                this@NewsWireViewModel.newsWireState.value = this@NewsWireViewModel.newsWireState.value.copy(
-                                    error = result.message
-                                )
+                            is Resource.Error -> {
+                                this@NewsWireViewModel.newsWireState.value =
+                                    this@NewsWireViewModel.newsWireState.value.copy(
+                                        error = result.message
+                                    )
                             }
 
                             is Resource.Success -> {
-                                this@NewsWireViewModel.newsWireState.value = this@NewsWireViewModel.newsWireState.value.copy(
-                                    newsWireArticles =result.data?.articles,
-                                    isLoading = false,
-                                    section = result.data?.section
-                                )
+                                this@NewsWireViewModel.newsWireState.value =
+                                    this@NewsWireViewModel.newsWireState.value.copy(
+                                        newsWireArticles = result.data?.articles,
+                                        isLoading = false,
+                                        section = result.data?.section
+                                    )
                             }
                         }
                     }.launchIn(this)
@@ -129,22 +133,27 @@ class NewsWireViewModel @Inject constructor(
 
     }
 
-    fun launchArticleInBrowser(context: Context,link:String){
+    fun launchArticleInBrowser(context: Context, link: String) {
         viewModelScope.launch {
-                val intent=Intent(Intent.ACTION_VIEW).apply {
-                    setData(Uri.parse(link))
-                }
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                setData(Uri.parse(link))
+            }
             context.startActivity(intent)
         }
     }
 
-    fun shareNewsArticle(context:Context,link:String){
+    fun shareNewsArticle(context: Context, link: String) {
         viewModelScope.launch {
-            val intent=Intent(Intent.ACTION_SEND).apply {
-                type="text/html"
-                putExtra(Intent.EXTRA_TEXT,link)
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/html"
+                putExtra(Intent.EXTRA_TEXT, link)
             }
-            context.startActivity(Intent.createChooser(intent,context.getString(R.string.share_txt)))
+            context.startActivity(
+                Intent.createChooser(
+                    intent,
+                    context.getString(R.string.share_txt)
+                )
+            )
         }
 
     }
